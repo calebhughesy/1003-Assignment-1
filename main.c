094416ec343ea5cb6e5727b4da7e8dfe978044b7
Caleb Hughes <c3328308@uon.edu.au>
2019-04-11 09:58:49 +0000

    Menu section was further developed, warnings still present for values scanned from the reader.

--- main.c
+++ main.c
@@ -2,14 +2,23 @@
 
 int main ()
 {
-    char Rotation;
-    char Substitution;
-    
-char method[2] = {Rotation, Substitution};
-  printf("Please choose from one of the following encryption methods:\n - Rotation\n - Substitution\n\n");
-  scanf("%s", method);
+    int Rotation = 1, Substitution = 2; /*pre-determined values
+                                          of rotation and substitution for memu */
+    int choice;
+    char method, Encrypt, Decrypt;
+    printf("Please choose from the following methods:\n1. Rotation\n2. Substitution\n");
+    scanf("%d", &choice);
+  printf("Option %d was chosen. Would you like to encrypt or decrpyt?\n", method);
+  scanf("%c", &method);
   
-  printf("%s was chosen. Would you like to encrypt or decrpyt?\n", method);
+  switch (method)
+  {
+      case 1: printf("Encryption was chosen.\n");
+      break;
+      case 2:  printf("Decryption was chosen.\n");
+      default: printf("Choice not identified.\n");
+      break;
+  }
   
   return 0;
 }
