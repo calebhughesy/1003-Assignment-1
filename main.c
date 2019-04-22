765e3fd624419fd3edb140ee44fed20e1303088a
Caleb Hughes <c3328308@uon.edu.au>
2019-04-22 04:12:52 +0000

    Implementing code into switch statement, altering menu.

--- main.c
+++ main.c
@@ -1,24 +1,81 @@
 #include <stdio.h>
-
 int main ()
 {
-    int Rotation = 1, Substitution = 2; /*pre-determined values
-                                          of rotation and substitution for memu */
     int choice;
-    char method, Encrypt, Decrypt;
-    printf("Please choose from the following methods:\n1. Rotation\n2. Substitution\n");
-    scanf("%d", &choice);
-  printf("Option %d was chosen. Would you like to encrypt or decrpyt?\n", method);
-  scanf("%c", &method);
-  
-  switch (method)
-  {
-      case 1: printf("Encryption was chosen.\n");
-      break;
-      case 2:  printf("Decryption was chosen.\n");
-      default: printf("Choice not identified.\n");
-      break;
-  }
-  
-  return 0;
+    
+        printf("Please choose from one of the following options:\n\n");
+        printf("1. Encryption with rotation\n");
+        printf("2. Decryption with rotation\n");
+        printf("3. Encryption with substitution\n");
+        printf("4. Decyrption with substitution\n");
+        scanf("%d", &choice);// value is read from "input" file.
+        
+    switch (choice)
+    {
+        case 1: printf("\nOption 1 selected.\n");
+        {
+	char message[] = "input.txt", msg, ch;
+	int i, key;
+	
+	//FILE *inFile;
+	//inFile = fopen(message, "r");
+	//fscanf(inFile, "%c", &msg);
+	printf("\nEnter a message to encrypt: \n");
+	gets(message);
+	printf("Enter shift key: \n");
+	scanf("%d", &key);
+	
+	for(i = 0; message[i] != '\0'; ++i){
+		ch = message[i];
+		
+	
+		if(ch >= 'A' && ch <= 'Z'){
+			ch = ch + key;
+			
+			if(ch > 'Z'){
+				ch = ch - 'Z' + 'A' - 1;
+			}
+			
+			message[i] = ch;
+		}
+	}
+	
+	printf("Encrypted message: %s", message);
+	
+	return 0;
+}
+        case 2: printf("\nOption 2 selected.\n");
+        {
+	char message[100], ch;
+	int i, key;
+	
+	printf("Enter a message to decrypt (CAPITAL LETTERS ONLY): ");
+	gets(message);
+	//scanf("%s", message);
+	printf("Enter shift key: ");
+	scanf("%d", &key);
+	
+	for(i = 0; message[i] != '\0'; ++i){
+		ch = message[i];
+		
+		if(ch >= 'A' && ch <= 'Z'){
+			ch = ch + key;
+			
+			message[i] = ch;
+		}
+	}
+	
+	printf("Original message: %s", message);
+	
+	return 0;
+}
+        break;
+        case 3: printf("Option 3 selected.\n");
+        break;
+        case 4: printf("Option 4 selected.\n");
+        break;
+        default: printf("Error - option not identified.");/* for any
+        value other than 1-4 */
+    }
+    return 0;
 }
